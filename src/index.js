3b3cdf415383e8e93f394951567aed22fa24f856
ivanik <ivan170102@gmail.com>
2019-02-05 13:05:48 +0500

    Clean up index.js

--- src/index.js
+++ src/index.js
@@ -2,47 +2,20 @@ import getTimetablesFileList from "./steps/getTimetablesFileList";
 import removeIgnored from "./steps/removeIgnored";
 import downloadAndParse from "./steps/downloadAndParse";
 import { timetable } from "../urls.json";
-import Timetable from "./models/timetable";
+
 import mongoose from "./lib/mongoose";
-import period from "./lib/period";
-import addDays from "./lib/addDays";
-
-const baseDate = new Date("2019-02-04T00:00:00.000Z");
-
-const t = async file => {
-  try {
-    const result = await downloadAndParse(file);
-
-    const three = result.timetable[period(baseDate)];
-    const tasks = [];
-    for (const day in three) {
-      if (({}.hasOwnProperty.call(three), day)) {
-        const document = new Timetable({
-          date: addDays(baseDate, parseInt(day, 10)),
-          group: result.name,
-          pairs: three[day]
-        });
-        tasks.push(document.save());
-      }
-    }
-    await Promise.all(tasks);
-  } catch (error) {
-    console.log(`${error.message} in ${file.file}`);
-  }
-};
 
 (async () => {
   const startTime = new Date();
+
   let fileList = await getTimetablesFileList(timetable.urls);
+
   console.log(`Found ${fileList.length} timetables files`);
   fileList = removeIgnored(fileList, timetable.ignore);
 
-  const tasks = [];
-  for (const file of fileList) {
-    tasks.push(t(file));
-  }
   console.log("Processing timetables");
-  await Promise.all(tasks);
+  await downloadAndParse(fileList);
+
   console.log(`Done in ${new Date() - startTime}ms`);
   mongoose.connection.close();
 })();
--- src/steps/downloadAndParse.js
+++ src/steps/downloadAndParse.js
@@ -1,7 +1,39 @@
 import download from "../fetch/download";
 import parseTimetable from "../parse/timeTable";
+import period from "../lib/period";
+import addDays from "../lib/addDays";
+import Timetable from "../models/timetable";
 
-export default async file => {
-  const book = await download(file.url);
-  return parseTimetable(file.file, book);
+const baseDate = new Date("2019-02-04T00:00:00.000Z");
+
+const make = async file => {
+  try {
+    const book = await download(file.url);
+    const result = parseTimetable(file.file, book);
+
+    const three = result.timetable[period(baseDate)];
+    const tasks = [];
+    for (const day in three) {
+      if (({}.hasOwnProperty.call(three), day)) {
+        const document = new Timetable({
+          date: addDays(baseDate, parseInt(day, 10)),
+          group: result.name,
+          pairs: three[day]
+        });
+        tasks.push(document.save());
+      }
+    }
+    await Promise.all(tasks);
+  } catch (error) {
+    console.log(`${error.message} in ${file.file}`);
+  }
+};
+
+export default async fileList => {
+  const tasks = [];
+  for (const file of fileList) {
+    tasks.push(make(file));
+  }
+
+  await Promise.all(tasks);
 };
