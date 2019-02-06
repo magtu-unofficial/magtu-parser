41c53eb139a814253d29ba8fcdf3a3878e7567fa
ivanik <ivan170102@gmail.com>
2019-02-06 20:03:24 +0500

    Another fix date LOL

--- src/changes/getChangesFile.js
+++ src/changes/getChangesFile.js
@@ -13,11 +13,7 @@ export default async url => {
         });
 
       if (numbers.length === 3 && numbers.findIndex(e => isNaN(e)) === -1) {
-        const date = new Date(
-          numbers[2] + 2000,
-          numbers[1] - 1,
-          numbers[0] + 1
-        );
+        const date = new Date(numbers[2] + 2000, numbers[1] - 1, numbers[0]);
 
         if (!result.date || result.date < date) {
           result.date = date;
