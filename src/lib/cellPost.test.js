124a9c07df092f5d76820bc03140ce87ba19e3c1
ivanik <ivan170102@gmail.com>
2019-02-27 20:15:44 +0500

    Добавлены тесты для AddDays, cellPos и period

---
+++ src/lib/cellPost.test.js
@@ -0,0 +1,25 @@
+import cellPos from "./cellPos";
+
+test("Ячейка A1", () => {
+  expect(cellPos(1, 1)).toBe("A1");
+});
+
+test("Ячейка Z10", () => {
+  expect(cellPos(26, 10)).toBe("Z10");
+});
+
+test("Ячейка AA15", () => {
+  expect(cellPos(27, 15)).toBe("AA15");
+});
+
+test("Ячейка AC10", () => {
+  expect(cellPos(29, 10)).toBe("AC10");
+});
+
+test("Ячейка ZZ2", () => {
+  expect(cellPos(702, 2)).toBe("ZZ2");
+});
+
+test("Ячейка BXX2", () => {
+  expect(cellPos(2000, 2)).toBe("BXX2");
+});
