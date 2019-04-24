ab148c883b0a614400bfb5e49a5c6bc0a00bd1a4
ivanik <ivan170102@gmail.com>
2019-04-24 09:03:35 +0500

    Завершаться при необработаной ошибке

--- src/index.ts
+++ src/index.ts
@@ -12,53 +12,57 @@ import mongoose from "./lib/mongoose";
 console.log("Starting parser");
 
 (async () => {
-  const startTime = new Date();
-  const launch = new Launch({ date: startTime });
+  try {
+    const startTime = new Date();
+    const launch = new Launch({ date: startTime });
 
-  console.log("Checking changes");
-  const changesComp = await Promise.all([
-    Launch.findlastChanges(),
-    getChangesFile(changes.url)
-  ]);
+    console.log("Checking changes");
+    const changesComp = await Promise.all([
+      Launch.findlastChanges(),
+      getChangesFile(changes.url)
+    ]);
 
-  console.log(
-    changesComp[0] < changesComp[1].date,
-    changesComp[0],
-    changesComp[1].date
-  );
+    console.log(
+      changesComp[0] < changesComp[1].date,
+      changesComp[0],
+      changesComp[1].date
+    );
 
-  if (changesComp[0] < changesComp[1].date) {
-    const { date, book } = changesComp[1];
-    launch.lastChanges = date;
-    launch.newChanges = true;
+    if (changesComp[0] < changesComp[1].date) {
+      const { date, book } = changesComp[1];
+      launch.lastChanges = date;
+      launch.newChanges = true;
 
-    console.log("New changes", date);
+      console.log("New changes", date);
 
-    const result = await timetable(date);
-    launch.files = result;
+      const result = await timetable(date);
+      launch.files = result;
 
-    const ch = parseChanges(book);
-    await Timetable.applyChanges(ch, date);
+      const ch = parseChanges(book);
+      await Timetable.applyChanges(ch, date);
 
-    try {
-      await fetch(`${bot}/notyfy`);
-    } catch (error) {
-      console.log(`Не получилось оповестить бота: ${error.message}`);
+      try {
+        await fetch(`${bot}/notyfy`);
+      } catch (error) {
+        console.log(`Не получилось оповестить бота: ${error.message}`);
+      }
+    } else {
+      const date = changesComp[0];
+      launch.lastChanges = date;
+      launch.newChanges = false;
+
+      console.log("No new changes");
     }
-  } else {
-    const date = changesComp[0];
-    launch.lastChanges = date;
-    launch.newChanges = false;
 
-    console.log("No new changes");
+    const endTime = new Date().getTime() - startTime.getTime();
+    launch.time = endTime;
+    console.log(`Done in ${endTime}ms`);
+    await launch.save();
+
+    console.log(`Disconecting from db`);
+    mongoose.connection.close();
+    console.log(`Disconected`);
+  } catch (error) {
+    console.log(`Необраьотаная ошибка: ${error.message}`);
   }
-
-  const endTime = new Date().getTime() - startTime.getTime();
-  launch.time = endTime;
-  console.log(`Done in ${endTime}ms`);
-  await launch.save();
-
-  console.log(`Disconecting from db`);
-  mongoose.connection.close();
-  console.log(`Disconected`);
 })();
