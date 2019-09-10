b72e9fd0e72e57fc19ac7a22b127cfa14556975b
ivanik <ivan170102@gmail.com>
2019-09-10 19:08:31 +0500

    Исправлен regexp кабината

--- src/utils/regexp.ts
+++ src/utils/regexp.ts
@@ -1,4 +1,4 @@
 export const groupG = /[А-Яа-я]{1,6}-\d{2}-\d|\d{2}ИП-.{1,6}/gi;
 export const date = /(\d{2})\.(\d{2})\.(\d{2})/i;
-export const classroom = /(А|М|У|П|Ин|О|Б|Л|С)[ -]?\d{1,3}(а|б)?| /;
+export const classroom = /(А|М|У|П|Ин|О|Б|Л|С)[ -]?\d{1,3}(а|б)?/;
 export const teacher = /[А-ЯЁ][а-яё]{2,} {1,2}[А-ЯЁ][а-яё]?\.[А-ЯЁ]\.?/;
