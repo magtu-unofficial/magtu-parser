e3f8aa731b8eb2a638d5ce4b3d759208f684c3b9
ivan <ivan170102@gmail.com>
2024-11-20 21:32:22 +0500

    Buypass bot protection

--- src/utils/files.ts
+++ src/utils/files.ts
@@ -69,7 +69,31 @@ export const fileList = async (
     });
   }
   if (from === Efrom.newlms) {
-    const res = await fetch(path);
+    const res = await fetch(path, {
+      headers: {
+        accept:
+          "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+        "accept-language": "en-US,en;q=0.9,ru;q=0.8",
+        "cache-control": "no-cache",
+        pragma: "no-cache",
+        "sec-ch-ua":
+          '"Google Chrome";v="131", "Chromium";v="131", "Not_A Brand";v="24"',
+        "sec-ch-ua-mobile": "?0",
+        "sec-ch-ua-platform": '"Windows"',
+        "sec-fetch-dest": "document",
+        "sec-fetch-mode": "navigate",
+        "sec-fetch-site": "same-origin",
+        "sec-fetch-user": "?1",
+        "upgrade-insecure-requests": "1",
+        cookie:
+          "BPC=af13c4e4925aeb3594d3fc55908de263; MoodleSession=d7emmp5bioeujndcjuj738ho0q",
+        Referer: "https://newlms.magtu.ru/course/view.php?id=26619",
+        "Referrer-Policy": "strict-origin-when-cross-origin"
+      },
+      body: null,
+      method: "GET"
+    });
+
     const dom = new JSDOM(await res.text());
     const elements = dom.window.document.querySelectorAll(
       ".fp-filename-icon a"
@@ -78,11 +102,7 @@ export const fileList = async (
     const files: Array<File> = [];
     elements.forEach(value => {
       files.push(
-        new File(
-          value.innerHTML,
-          value.getAttribute("href"),
-          urls.from
-        )
+        new File(value.innerHTML, value.getAttribute("href"), urls.from)
       );
     });
     return files;
